768c231febab42e4667228b270e847ed01ee3d0c
genie <mgenie93@gmail.com>
2024-05-08 16:33:49 +0900

    vue3 basic

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <AppHeader v-bind:appTitle="message" v-on:change="changeMassage"> </AppHeader>
+</template>
+
+<script>
+// import 컴포넌트이름 from './컴포넌트 경로'
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    // '컴포넌트이름' : 컴포넌트 내용
+    //AppHeader: AppHeader,
+    // 앞과 뒤가 같으면 줄어든다(키:밸류)
+    AppHeader,
+  },
+  data() {
+    return {
+      message: "앱 헤더 컴포넌트",
+    };
+  },
+  methods: {
+    changeMassage() {
+      this.message = "변경됨";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue3-form/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <form action="" @:submit.prevent="submitForm">
+    <!-- v-on -->
+    <div>
+      <label for="userId">ID: </label>
+      <input id="userId" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW: </label>
+      <input type="text" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+import { ref } from "vue";
+export default {
+  setup() {
+    // data와 완전하게 동일하다.
+    var username = ref("");
+    var password = ref("");
+
+    // methods
+    var submitForm = () => {
+      axios
+        .post("https://jsonplaceholder.typicode.com/users/", {
+          username: username.value,
+          password: password.value,
+        })
+        .then((Response) => {
+          console.log(Response);
+        });
+    };
+    return { username, password, submitForm };
+  },
+
+  // methods:{
+  //   logText(){
+  //     this.password
+  //   }
+  // }
+
+  // data() {
+  //   return {
+  //     username: "",
+  //     password: "",
+  //   };
+  // },
+  // methods: {
+  //   submitForm() {
+  //     // event.preventDefault();
+  //     const data = {
+  //       username: this.username,
+  //       password: this.password,
+  //     };
+  //     axios
+  //       .post("https://jsonplaceholder.typicode.com/users/", data)
+  //       .then((Response) => {
+  //         console.log(Response);
+  //       });
+  //     console.log("제출됨");
+  //   },
+  // },
+};
+</script>
+
+<style scoped></style>
